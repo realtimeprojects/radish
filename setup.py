c220aaddc08c183e2a6a69af30e7901c84f293ee
Timo Furrer <tuxtimo@gmail.com>
2012-09-16 17:14:42 +0200

    Added setuptools environment

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+  name = "radish",
+  version = "0.00.01",
+  description = "Behaviour Driven Development tool for python",
+  author = "Timo Furrer",
+  author_email = "tuxtimo@gmail.com",
+  url = "http://github.com/timofurrer/radish",
+  packages = [ "radish" ],
+  entry_points = { "console_scripts": [ "radish = radish.main:main", ] },
+  package_data = { "radish": [ "*.md" ] }
+)
